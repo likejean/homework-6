cb659b3393f03674f314eaf6f4313271d80ff029
likejean <popachs@yahoo.com>
2020-04-10 02:53:50 -0700

    Updates

---
+++ precache-manifest.0d5d3d0f3acfc11c88f4c8a2d198069c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029415a27593d2f96e8c1a79a94e0ab8",
+    "url": "/homework-6/index.html"
+  },
+  {
+    "revision": "a25b812ddd44cfa6a66f",
+    "url": "/homework-6/static/css/2.2ef4d3d3.chunk.css"
+  },
+  {
+    "revision": "85b93a79b8a5e6b24a34",
+    "url": "/homework-6/static/css/main.31b14488.chunk.css"
+  },
+  {
+    "revision": "a25b812ddd44cfa6a66f",
+    "url": "/homework-6/static/js/2.f4acc4d3.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/homework-6/static/js/2.f4acc4d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b93a79b8a5e6b24a34",
+    "url": "/homework-6/static/js/main.85496bdb.chunk.js"
+  },
+  {
+    "revision": "9c9df88d8febc51b3bf3",
+    "url": "/homework-6/static/js/runtime-main.ff0234c0.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/homework-6/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/homework-6/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/homework-6/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/homework-6/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/homework-6/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/homework-6/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/homework-6/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/homework-6/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/homework-6/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/homework-6/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/homework-6/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/homework-6/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/homework-6/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/homework-6/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/homework-6/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/homework-6/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/homework-6/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
