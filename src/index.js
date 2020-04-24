9bdc4a2905ac78e7f297d92c83f2abe30801dd00
likejean <popachs@yahoo.com>
2020-04-23 18:56:01 -0700

    Deploying App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider as AlertProvider } from 'react-alert';
+import {Provider as AlertProvider} from 'react-alert';
 import App from './App';
-import { options } from './helpers/AlertOptions';
+import {options} from './helpers/AlertOptions';
 import AlertTemplate from 'react-alert-template-basic';
 
 ReactDOM.render(
-    <AlertProvider template={AlertTemplate} {...options}>
-        <App/>
-    </AlertProvider>, document.getElementById('root'));
+        <AlertProvider template={AlertTemplate} {...options}>
+            <App/>
+        </AlertProvider>,
+    document.getElementById('root'));
 
